0b845fa3ce95ef3f04ad823d864e738adb6932d0
Nícolas Carvalho <nicolassprofissional@gmail.com>
2024-01-03 12:12:04 -0300

    automating text fields

---
+++ send_form.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+class SendForm:
+
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(self.options)
+        self.driver.get("https://demoqa.com/automation-practice-form")
+
+    def first_name(self, name: str):
+        element = self.driver.find_element(By.XPATH, '//*[@id="firstName"]')
+        element.send_keys(name)
+
+    def last_name(self, last_name: str):
+        element = self.driver.find_element(By.XPATH, '//*[@id="lastName"]')
+        element.send_keys(last_name)
+
+    def email(self, email: str):
+        element = self.driver.find_element(By.XPATH, '//*[@id="userEmail"]')
+        element.send_keys(email)
+
+    def gender(self):
+        pass
+
+    def mobile(self, mobile: str):
+        element = self.driver.find_element(By.XPATH, '//*[@id="userNumber"]')
+        element.send_keys(mobile)
+
+    def date_of_birth(self):
+        pass
+
+    def subjects(self, subjects):
+        element = self.driver.find_element(By.XPATH, '//*[@id="subjectsInput"]')
+        element.send_keys(subjects)
+
+    def hobbies(self):
+        pass
+
+    def picture(self):
+        pass
+
+    def current_address(self, current_address: str):
+        element = self.driver.find_element(By.XPATH, '//*[@id="currentAddress"]')
+        element.send_keys(current_address)
+
+    def state_and_city(self):
+        pass
+
+    def close_website(self):
+        self.driver.close()
