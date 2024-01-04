a5eb7f5ac85cb727ac7937ff6e4eddbd4185d5b6
Nícolas Carvalho <nicolassprofissional@gmail.com>
2024-01-04 19:53:11 -0300

    adding assertTrue

--- test_cases_script.py
+++ test_cases_script.py
@@ -2,88 +2,88 @@ import unittest
 from send_form import SendForm
 from selenium import webdriver
 
-class TestCasesScrpit(unittest.TestCase):
+class TestCasesScript(unittest.TestCase):
 
     def setUp(self):
         self.form = SendForm()
 
     def test_complete_form(self):
         try:
-            self.form.first_name(name='abacaxi')
-            self.form.last_name(last_name='laranja')
-            self.form.email(email='abacaxilaranja@gmail.com')
-            self.form.gender(gender=1)
-            self.form.mobile(mobile='1988370591')
-            self.form.date_of_birth(day=21, month=12, year=2003)
-            self.form.subjects(subjects='bla bla bla')
-            self.form.hobbies(hobbies=2)
-            self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png')
-            self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024')
-            self.form.submit_and_close_website()
+            self.assertTrue(self.form.first_name(name='abacaxi'))
+            self.assertTrue(self.form.last_name(last_name='laranja'))
+            self.assertTrue(self.form.email(email='abacaxilaranja@gmail.com'))
+            self.assertTrue(self.form.gender(gender=1))
+            self.assertTrue(self.form.mobile(mobile='1988370591'))
+            self.assertTrue(self.form.date_of_birth(day=21, month=12, year=2003))
+            self.assertTrue(self.form.subjects(subjects='bla bla bla'))
+            self.assertTrue(self.form.hobbies(hobbies=2))
+            self.assertTrue(self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png'))
+            self.assertTrue(self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024'))
+            self.assertTrue(self.form.submit_and_close_website())
         except Exception as log:
             print(f'Erro desconhecido: {log}')
 
     def test_incomplete_name_field(self):
         try:
-            self.form.first_name(name='')
-            self.form.last_name(last_name='laranja')
-            self.form.email(email='abacaxilaranja@gmail.com')
-            self.form.gender(gender=1)
-            self.form.mobile(mobile='1988370591')
-            self.form.date_of_birth(day=21, month=12, year=2003)
-            self.form.subjects(subjects='bla bla bla')
-            self.form.hobbies(hobbies=2)
-            self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png')
-            self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024')
-            self.form.submit_and_close_website()
+            self.assertTrue(self.form.first_name(name=''))
+            self.assertTrue(self.form.last_name(last_name='laranja'))
+            self.assertTrue(self.form.email(email='abacaxilaranja@gmail.com'))
+            self.assertTrue(self.form.gender(gender=1))
+            self.assertTrue(self.form.mobile(mobile='1988370591'))
+            self.assertTrue(self.form.date_of_birth(day=21, month=12, year=2003))
+            self.assertTrue(self.form.subjects(subjects='bla bla bla'))
+            self.assertTrue(self.form.hobbies(hobbies=2))
+            self.assertTrue(self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png'))
+            self.assertTrue(self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024'))
+            self.assertTrue(self.form.submit_and_close_website())
         except Exception as log:
             print(f'Erro encontrado: {log}')
 
     def test_incomplete_lastname_field(self):
         try:
-            self.form.first_name(name='abacaxi')
-            self.form.last_name(last_name='')
-            self.form.email(email='abacaxilaranja@gmail.com')
-            self.form.gender(gender=1)
-            self.form.mobile(mobile='1988370591')
-            self.form.date_of_birth(day=21, month=12, year=2003)
-            self.form.subjects(subjects='bla bla bla')
-            self.form.hobbies(hobbies=2)
-            self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png')
-            self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024')
-            self.form.submit_and_close_website()
+            self.assertTrue(self.form.first_name(name='abacaxi'))
+            self.assertTrue(self.form.last_name(last_name=''))
+            self.assertTrue(self.form.email(email='abacaxilaranja@gmail.com'))
+            self.assertTrue(self.form.gender(gender=1))
+            self.assertTrue(self.form.mobile(mobile='1988370591'))
+            self.assertTrue(self.form.date_of_birth(day=21, month=12, year=2003))
+            self.assertTrue(self.form.subjects(subjects='bla bla bla'))
+            self.assertTrue(self.form.hobbies(hobbies=2))
+            self.assertTrue(self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png'))
+            self.assertTrue(self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024'))
+            self.assertTrue(self.form.submit_and_close_website())
         except Exception as log:
             print(f'Erro encontrado: {log}')
 
     def test_incomplete_email_field(self):
         try:
-            self.form.first_name(name='abacaxi')
-            self.form.last_name(last_name='laranja')
-            self.form.email(email='abacaxilaranja')
-            self.form.gender(gender=1)
-            self.form.mobile(mobile='1988370591')
-            self.form.date_of_birth(day=21, month=12, year=2003)
-            self.form.subjects(subjects='bla bla bla')
-            self.form.hobbies(hobbies=2)
-            self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png')
-            self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024')
-            self.form.submit_and_close_website()
+            self.assertTrue(self.form.first_name(name='abacaxi'))
+            self.assertTrue(self.form.last_name(last_name='laranja'))
+            self.assertTrue(self.form.email(email='abacaxilaranja'))
+            self.assertTrue(self.form.gender(gender=1))
+            self.assertTrue(self.form.mobile(mobile='1988370591'))
+            self.assertTrue(self.form.date_of_birth(day=21, month=12, year=2003))
+            self.assertTrue(self.form.subjects(subjects='bla bla bla'))
+            self.assertTrue(self.form.hobbies(hobbies=2))
+            self.assertTrue(self.form.picture('D:\\Programacao\\BDD-Selenium-CuriosityAcademy\\user_image.png'))
+            self.assertTrue(self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024'))
+            self.assertTrue(self.form.submit_and_close_website())
         except Exception as log:
             print(f'Erro encontrado: {log}')
 
     def test_incomplete_image_field(self):
         try:
-            self.form.first_name(name='abacaxi')
-            self.form.last_name(last_name='laranja')
-            self.form.email(email='abacaxilaranja@gmail.com')
-            self.form.gender(gender=1)
-            self.form.mobile(mobile='1988370591')
-            self.form.date_of_birth(day=21, month=12, year=2003)
-            self.form.subjects(subjects='bla bla bla')
-            self.form.hobbies(hobbies=2)
-            self.form.picture('')
-            self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024')
-            self.form.submit_and_close_website()
+            self.assertTrue(self.form.first_name(name='abacaxi'))
+            self.assertTrue(self.form.last_name(last_name='laranja'))
+            self.assertTrue(self.form.email(email='abacaxilaranja@gmail.com'))
+            self.assertTrue(self.form.gender(gender=1))
+            self.assertTrue(self.form.mobile(mobile='1988370591'))
+            self.assertTrue(self.form.date_of_birth(day=21, month=12, year=2003))
+            self.assertTrue(self.form.subjects(subjects='bla bla bla'))
+            self.assertTrue(self.form.hobbies(hobbies=2))
+            self.assertTrue(self.form.picture(''))
+            self.assertTrue(self.form.current_address(current_address='Rua 04 casa n° 01, avenida 2024'))
+            self.assertTrue(self.form.submit_and_close_website())
         except Exception as log:
             print(f'Erro encontrado: {log}')
         
